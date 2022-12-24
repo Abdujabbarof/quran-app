0ae8c2779fa4955b47c56c147fa2cee3aefefc5d
Abdujabbarof <abdujabborov6172@gmail.com>
2022-12-24 20:16:50 +0500

    updated surahs

--- src/pages/Surahs.jsx
+++ src/pages/Surahs.jsx
@@ -7,25 +7,39 @@ import axios from 'axios'
 
 function Surahs() {
   const [data, setData] = useState()
-
-  const datas = () => {
-    axios.get('http://api.alquran.cloud/v1/surah')
-    .then(res => {
-      setData(res.data.data)
-    })
-  } 
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    datas()
+    axios.get("http://api.alquran.cloud/v1/surah")
+    .then((data) => {
+      setData(data.data.data);
+      setLoading(false)
+    })
   }, [])
-  
 
   return (
     <>
       <Header home />
       <section className='surahs'>
         <div className="container">
-          <Link className='card' to={'/surah'}>
+          {
+            loading ? <h1>Loading...</h1> : data.map(elem => (
+              <Link key={elem.number} className='card' to={'/surah'}>
+                <div className="left">
+                  <div className="num"><span>{elem.number}</span></div>
+                  <div className="text">
+                    <h3>{elem.englishName}</h3>
+                    <p>{elem.englishNameTranslation}</p>
+                  </div>
+                </div>
+                <div className="right">
+                  <h4>{elem.name}</h4>
+                  <p>{elem.revelationType}, {elem.numberOfAyahs} Ayahs</p>
+                </div>
+              </Link>
+            ))
+          }
+          {/* <Link className='card' to={'/surah'}>
               <div className="left">
                 <div className="num"><span>1</span></div>
                 <div className="text">
@@ -37,7 +51,7 @@ function Surahs() {
                 <h4>سُورَةُ ٱلْفَاتِحَةِ</h4>
                 <p>Meccan, 7 Ayahs</p>
               </div>
-          </Link>
+          </Link> */}
         </div>
       </section>
       <Footer />
