256f83c563854405558a20a87b991c26c4206814
Abdujabbarof <abdujabborov6172@gmail.com>
2022-12-25 17:10:46 +0500

    audio working

--- src/pages/Surah.jsx
+++ src/pages/Surah.jsx
@@ -5,23 +5,32 @@ import axios, { all } from 'axios'
 import { useParams } from 'react-router'
 
 const Surah = () => {
+    const { id } = useParams()
     const [ayah, setAyah] = useState()
     const [tran, setTrans] = useState()
-    const { id } = useParams()
+    const [audio, setAudio] = useState()
     const [loading, setLoading] = useState(true)
+    const play = new Audio()
     
     useEffect(() => {
         const ayahs = axios.get(`https://api.alquran.cloud/v1/surah/${id}`)
         const trans = axios.get(`https://api.alquran.cloud/v1/surah/${id}/uz.sodik`)
+        const audios = axios.get(`https://api.alquran.cloud/v1/surah/${id}/ar.alafasy`)
         
-        axios.all([ayahs, trans])
+        axios.all([ayahs, trans, audios])
         .then(axios.spread((...allData) => {
             setAyah(allData[0].data.data)
             setTrans(allData[1].data.data.ayahs)
+            setAudio(allData[2].data.data.ayahs)
             setLoading(false)
         }))
         .catch(err => alert("Something went wrong ("))
     }, [])
+    
+    const showNum = (e) => {
+        play.src = `${audio[e.target.parentElement.getAttribute("order") - 1].audio}`
+        play.play()
+    }
 
   return (
     <>
@@ -33,12 +42,12 @@ const Surah = () => {
             <div className="card-wrap">
                 {
                     loading ? <h1>Loading...</h1> : ayah.ayahs.map(item => (
-                        <div key={item.number} className="card">
+                        <div key={item.number} order={item.numberInSurah} className="card">
                             <h5>{ayah.number}:{item.numberInSurah}</h5>
                             <h1>{item.text} <span>{item.numberInSurah}</span></h1>
                             <h3>{tran[item.numberInSurah - 1].text}</h3>
 
-                            <button><i class="fa-solid fa-volume-high"></i> Play Audio</button>
+                            <button onClick={showNum}><i class="fa-solid fa-volume-high"></i> Play Audio</button>
                         </div>
                     ))
                 }
