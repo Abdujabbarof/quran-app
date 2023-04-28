72b99a6e81b17856523624db0dd8d958f16f7508
Abdujabbarof <abdujabborov6172@gmail.com>
2023-04-28 19:55:20 +0500

    a

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer>
         <div className="container">
-            <p>&copy; Copyright. Powered by <a href="https://www.albison.uz/" target="_blank">Albison</a></p>
+            <p>&copy; Copyright. Powered by <a href="https://www.instagram.com/_umar_mee/" target="_blank">MuhammadUmar</a></p>
             <p>Made with <i class="fa-solid fa-heart"></i> by <a href="https://www.instagram.com/_abdujabbarof_/">abdujabbarof</a></p>
         </div>
     </footer>
